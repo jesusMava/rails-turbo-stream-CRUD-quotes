74d08550f06c7d8ab5ffa879ebfe74b37b1dae6b
jesusMava <jesusmacedo7@gmail.com>
2026-04-08 16:24:43 -0600

    add turbo-stream security

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,11 +2,9 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
 
-  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  # after_update_commit -> { broadcast_replace_later_to "quotes" }
-  # after_destroy_commit -> { broadcast_remove_to "quotes" }
-  # Those three callbacks are equivalent to the following single line
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  # Users who share broadcastings should have the lambda return an array with the same values.
+  # Users who shouldn't share broadcastings should have the lambda return an array with different values.
+  broadcasts_to ->(quote) { [ quote.company, "quotes" ] }, inserts_by: :prepend
 
   belongs_to :company
 end
